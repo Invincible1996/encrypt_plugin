81859e195a03f77d799a27e2daf24e063fb5b7ed
zhangwenqiang <13701701451@163.com>
2022-01-27 10:57:44 +0800

    remove log

--- src/pages/TopForm.tsx
+++ src/pages/TopForm.tsx
@@ -8,56 +8,7 @@ export default function () {
   const [form] = Form.useForm();
   const [inputText, setText] = useState('');
   const [outputValue, setOutputValue] = useState('');
-  /**
-   *
-   * AES 加密
-   */
-  const encrypt = (type: number) => {
-    console.log(form.getFieldsValue());
-    // console.log(inputText);
-    const formData = form.getFieldsValue();
-    if (!formData.password) {
-      message.error('密码必填');
-      return;
-    }
-    const model = formData.model;
-    const password = formData.password; // 密钥
-    const output = formData.output; // 输出 base64 or hex
-    const charset = formData.charset; // 字符集
-    const offset = formData.offset; // 偏移量
-    const padding = formData.padding; // 填充
 
-    const iv = CryptoJS.enc.Utf8.parse(offset); //十六位十六进制数作为秘钥偏移量
-    const key = CryptoJS.enc.Utf8.parse(password); //十六位十六进制数作为秘钥
-    // const key = CryptoJS.enc.Utf8.parse("keykeykeykeykeyk"); //十六位十六进制数作为秘钥
-    // const iv = CryptoJS.enc.Utf8.parse('1234567887654321'); //十六位十六进制数作为秘钥偏移量
-
-    console.log(model);
-    console.log(padding);
-
-    console.log(CryptoJS.pad.Pkcs5);
-    console.log(CryptoJS.pad.Pkcs7);
-
-    if (type === 0) {
-      let message = secret.Encrypt(
-        inputText,
-        key,
-        iv,
-        modes[model],
-        paddingTypes[padding],
-      );
-      setOutputValue(message);
-    } else {
-      let message = secret.Decrypt(
-        inputText,
-        key,
-        iv,
-        modes[model],
-        paddingTypes[padding],
-      );
-      setOutputValue(message);
-    }
-  };
 
   const modes: { [index: string]: any } = {
     ECB: CryptoJS.mode.ECB,
@@ -84,15 +35,64 @@ export default function () {
 
   /**
    *
+   * AES 加密
+   */
+  const encrypt = (type: number) => {
+    console.log(form.getFieldsValue());
+    // console.log(inputText);
+    const formData = form.getFieldsValue();
+
+    if (!inputText) {
+      message.error('请输入要加密的内容');
+      return;
+    }
+
+    if (!formData.password) {
+      message.error('密码必填');
+      return;
+    }
+
+    const model = formData.model;
+    const password = formData.password; // 密钥
+    const output = formData.output; // 输出 base64 or hex
+    const charset = formData.charset; // 字符集
+    const offset = formData.offset; // 偏移量
+    const padding = formData.padding; // 填充
+
+    const iv = CryptoJS.enc.Utf8.parse(offset); //十六位十六进制数作为秘钥偏移量
+    const key = CryptoJS.enc.Utf8.parse(password); //十六位十六进制数作为秘钥
+    // const key = CryptoJS.enc.Utf8.parse("keykeykeykeykeyk"); //十六位十六进制数作为秘钥
+    // const iv = CryptoJS.enc.Utf8.parse('1234567887654321'); //十六位十六进制数作为秘钥偏移量
+
+    if (type === 0) {
+      let message = secret.Encrypt(
+        inputText,
+        key,
+        iv,
+        modes[model],
+        paddingTypes[padding],
+      );
+      setOutputValue(message);
+    } else {
+      let message = secret.Decrypt(
+        inputText,
+        key,
+        iv,
+        modes[model],
+        paddingTypes[padding],
+      );
+      setOutputValue(message);
+    }
+  };
+
+  /**
+   *
    *
    * @param e
    */
   const onChange = (e: any) => {
     setText(e.target.value);
   };
-
-  console.log(typeof CryptoJS.pad.ZeroPadding);
-  console.log(typeof CryptoJS.mode.ECB);
 
   return (
     <div>
@@ -114,7 +114,7 @@ export default function () {
           }}
         >
           <Form.Item label="AES加密模式：" name="model">
-            <Select style={{ width: 100 }}>
+            <Select style={{width: 100}}>
               {Object.keys(modes).map((item, index) => (
                 <Select.Option key={item} value={item}>
                   {item}
@@ -123,7 +123,7 @@ export default function () {
             </Select>
           </Form.Item>
           <Form.Item label="填充：" name="padding">
-            <Select style={{ width: 140 }}>
+            <Select style={{width: 140}}>
               {Object.keys(paddingTypes).map((item, index) => (
                 <Select.Option key={item} value={item}>
                   {item}
@@ -132,7 +132,7 @@ export default function () {
             </Select>
           </Form.Item>
           <Form.Item label="数据块：" name="dataModule">
-            <Select style={{ width: 100 }}>
+            <Select style={{width: 100}}>
               {dataModules.map((item) => (
                 <Select.Option
                   key={item}
@@ -142,13 +142,13 @@ export default function () {
             </Select>
           </Form.Item>
           <Form.Item label="密码：" name="password">
-            <Input placeholder="请输入密码" />
+            <Input placeholder="请输入密码"/>
           </Form.Item>
           <Form.Item label="偏移量：" name="offset">
-            <Input placeholder="iv偏移量,ecb模式不需要" />
+            <Input placeholder="iv偏移量,ecb模式不需要"/>
           </Form.Item>
           <Form.Item label="输出：" name="output">
-            <Select style={{ width: 100 }}>
+            <Select style={{width: 100}}>
               {outputs.map((item) => (
                 <Select.Option
                   key={item}
@@ -158,7 +158,7 @@ export default function () {
             </Select>
           </Form.Item>
           <Form.Item label="字符集：" name="charset">
-            <Select style={{ width: 150 }}>
+            <Select style={{width: 150}}>
               {chartList.map((item) => (
                 <Select.Option
                   key={item}
@@ -170,10 +170,10 @@ export default function () {
         </Form>
       </div>
       <div>
-        <div style={{ paddingTop: 10, paddingLeft: 0, paddingRight: 0 }}>
+        <div style={{paddingTop: 10, paddingLeft: 0, paddingRight: 0}}>
           <span>待加密、解密的文本</span>
-          <div style={{ paddingTop: 10 }}>
-            <Input.TextArea rows={10} onChange={onChange} />
+          <div style={{paddingTop: 10}}>
+            <Input.TextArea rows={10} onChange={onChange}/>
           </div>
         </div>
         <div
@@ -188,15 +188,15 @@ export default function () {
           <Button type="primary" onClick={() => encrypt(0)}>
             AES加密
           </Button>
-          <div style={{ width: 20 }}></div>
+          <div style={{width: 20}}></div>
           <Button type="primary" onClick={() => encrypt(1)}>
             AES解密
           </Button>
         </div>
-        <div style={{ paddingTop: 20, paddingLeft: 0, paddingRight: 0 }}>
+        <div style={{paddingTop: 20, paddingLeft: 0, paddingRight: 0}}>
           <span>AES加密、解密转换结果(base64了)</span>
-          <div style={{ paddingTop: 10 }}>
-            <Input.TextArea rows={10} value={outputValue} />
+          <div style={{paddingTop: 10}}>
+            <Input.TextArea rows={10} value={outputValue}/>
           </div>
         </div>
       </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,11 @@
-import {Form} from 'antd';
-import {useState} from 'react';
 // @ts-ignore
 import TopForm2 from './TopForm2';
 import TopForm from "@/pages/TopForm";
 
 export default function IndexPage() {
-  const [expand, setExpand] = useState(false);
-  const [form] = Form.useForm();
-
   return (
     <div style={{padding: 20}}>
-      <h1>在线AES加密解密、AES在线加密解密、AES encryption and decryption</h1>
+      <h1>AES在线加密(encryption)解密(decryption)</h1>
       <TopForm/>
       {/*<TopForm2/>*/}
     </div>
