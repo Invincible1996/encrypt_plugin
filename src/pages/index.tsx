7f62e0306f45fdab5a21e71464b646f29edd0e96
zhangwenqiang <13701701451@163.com>
2022-01-27 10:52:38 +0800

    remove pkcs5

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,8 +11,8 @@ export default function IndexPage() {
   return (
     <div style={{padding: 20}}>
       <h1>在线AES加密解密、AES在线加密解密、AES encryption and decryption</h1>
-      {/*<TopForm/>*/}
-      <TopForm2/>
+      <TopForm/>
+      {/*<TopForm2/>*/}
     </div>
   );
 }
